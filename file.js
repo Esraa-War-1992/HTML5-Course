6b022fa11a970af88d8ee32da18c4e47f028dde8
SOSO <asraa.a2z.92@gmail.com>
2021-10-11 17:28:11 +0200

    init video 27 to video 28

---
+++ file.js
@@ -0,0 +1,28 @@
+var MyCanvas = document.getElementById('c');
+
+myContext   = MyCanvas.getContext('2d');
+
+myContext.fillStyle = '#FF0';
+myContext.fillRect(0, 0, 800, 400,);
+
+ myContext.fillStyle = '#080';
+
+myContext.fillRect( 10 , 10 , 100 , 50 );
+
+myContext.fillStyle = '#f00';
+
+myContext.fillRect( 20 , 20 , 100 , 50 );
+
+myContext.fillStyle = '#00f';
+
+myContext.fillRect( 30 , 30 , 100 , 50 );
+
+
+
+myContext.lineWidth = 10;
+
+// myContext.strokeRect( 100, 100, 100, 50);
+//
+
+
+// myContext.clearRect(10, 10, 100, 100);
