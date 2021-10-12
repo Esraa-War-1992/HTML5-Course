ffecde410708177992a83f73edfbc9179ede9393
SOSO <asraa.a2z.92@gmail.com>
2021-10-12 17:55:17 +0200

    init video 27 to video 28

--- assets/js/video-28.js
+++ assets/js/video-28.js
@@ -3,7 +3,8 @@ var MyCanvas = document.getElementById('c');
 myContext   = MyCanvas.getContext('2d');
 
 myContext.fillStyle = '#FF0';
-myContext.fillRect(0, 0, 800, 400,);
+
+myContext.fillRect(0, 0, 900, 600);
 
 myContext.fillStyle = '#080';
 
@@ -17,12 +18,8 @@ myContext.fillStyle = '#00f';
 
 myContext.fillRect( 30 , 30 , 100 , 50 );
 
-
-
-myContext.lineWidth = 10;
+myContext.fillStyle = '#080';
 
 // myContext.strokeRect( 100, 100, 100, 50);
-//
-
 
 // myContext.clearRect(10, 10, 100, 100);
