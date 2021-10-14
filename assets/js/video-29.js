2ddb58c21f326ea27b62e48e3243f8c52807e4ff
SOSO <asraa.a2z.92@gmail.com>
2021-10-14 17:02:08 +0200

    init video 28 to video 29

--- assets/js/video-29.js
+++ assets/js/video-29.js
@@ -1,5 +1,45 @@
  var myCanvas = document.getElementById('c');
 
+    myContext = myCanvas.getContext('2d'),
 
+       myCanvaswidth = myCanvas.width;
 
-   myContext = myCanvas.getContext('2d');
+           myCanvasHight = myCanvas.height;
+
+             myContext.fillStyle = '#ddd'; // Fill Color
+
+               myContext.fillRect(0, 0, myCanvaswidth, myCanvasHight); // Rectangle Filled Inside Canvas
+
+                  myContext.strokeStyle = '#00f';
+
+                     myContext.lineWidth = 10 ;
+
+            // Top Left Line
+
+                    myContext.moveTo(10,10);
+
+                      myContext.lineTo(130 , 130);
+
+           // Top Right Line
+
+                   myContext.moveTo(290,10);
+
+                     myContext.lineTo(170 , 130);
+
+                           myContext.stroke();
+
+            // Bottom Left Line
+
+                 myContext.moveTo(10,290);
+
+                     myContext.lineTo(130 , 170);
+
+                        myContext.stroke();
+
+            // Bottom Right Line
+
+                 myContext.moveTo(290,290);
+
+                     myContext.lineTo(170 , 170);
+
+                         myContext.stroke();
